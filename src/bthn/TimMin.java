ca288fd73c6fa8b06f20fa68090ac4d01804bb4d
thuybeo03 <thuynmph22235@fpt.edu.vn>
2024-08-15 23:51:13 +0700

    bai tap 16-08-2024

--- src/bthn/TimMin.java
+++ src/bthn/TimMin.java
@@ -4,7 +4,7 @@ public class TimMin {
     // 1 ham truyen 2 so -> tra so be hon
     // 1 ham truyen 2 so -> tra so lon hon
     // in so chan trong mang
-    // tim max la so chan trong mang
-    // tim min la so chan trong mang
+    // tim max la so chan trong mang -> ve ra paint
+    // tim min la so chan trong mang -> ve ra paint
 
 }
--- src/bthn/timMaxSoChan.java
+++ src/bthn/timMaxSoChan.java
@@ -5,13 +5,12 @@ public class timMaxSoChan {
         int[] nums = { 0, 3, 5,8, 7, 13, 17, 23, 29,12 ,4,6};
         int max=0;
         for (int i = 0; i < nums.length; i++) {
-
             if(nums[i] % 2 == 0){
                 if(max<nums[i]){
                     max=nums[i];
-                    System.out.println("max" + max);
                 }
             }
         }
+        System.out.println("max = " + max);
     }
 }
